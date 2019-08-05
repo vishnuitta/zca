a02a562968b6a7f42e7bdf5db70ec49e9810dcd2
Jeffry Molanus <jeffry.molanus@gmail.com>
2019-08-05 10:24:01 +0200

    Initial commit

---
+++ tests/zpool.rs
@@ -0,0 +1,11 @@
+use libzfs::Libzfs;
+#[test]
+fn zpool_list_test() {
+    // this test assumes a pool called "test" exists
+
+    let mut zfs = Libzfs::new();
+    let pool = zfs.pool_by_name("test");
+    let pool = pool.unwrap();
+    let state = pool.state();
+    dbg!(pool);
+}
